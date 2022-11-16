feb7d85cb225370a5f979018eaa8f31c0ec63e8e
adarraillan <a.darraillan2@gmail.com>
2022-11-16 21:37:56 +0100

    Dealt the problem of comments, added tests

--- scrapper.py
+++ scrapper.py
@@ -1,19 +1,18 @@
+# -*- coding: utf-8 -*-
 import json
 
-from bs4 import BeautifulSoup
-import requests
-import re
-
+import requests, re
 import time
-from selenium.webdriver import Chrome#, Firefox
+import sys, getopt
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-
-tryWithTheComments = False
-
+tryWithTheComments = True
 
 def getTheRequestedUrl(video_id : str)-> requests.models.Response:
     return requests.get("https://www.youtube.com/watch?v=" + video_id)
@@ -57,17 +56,33 @@ def getTheExternalLinksWithinVideoDescription(soup : BeautifulSoup)-> list:
     return re.findall(r'(https?://\S+)', getTheVideoDescription(soup))
 
 def getTheVideoComments(video_id : str)-> list:
-    with Chrome() as driver:
-        wait = WebDriverWait(driver, 60)
-        driver.get("https://www.youtube.com/watch?v=" + video_id)
+    services = Service(ChromeDriverManager().install())
+    options = Options()
 
-        for item in range(0):
-            wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-            time.sleep(5)
+    driver = webdriver.Chrome(service=services, options=options)
+    driver.get("https://www.youtube.com/watch?v=" + video_id)
+    time.sleep(2)
 
-        return [comment.text for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#comment #content-text")))]
+    allTheComments = []
+    element = driver.find_element(By.XPATH, "//*[@id=\"comments\"]")
+    driver.execute_script("arguments[0].scrollIntoView();", element)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+    commentsList = soup.find_all("ytd-comment-thread-renderer", {"class": "style-scope ytd-item-section-renderer"}, limit = 5)
+    timer = 0
+    while commentsList == []:
+        time.sleep(1)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        commentsList = soup.find_all("ytd-comment-thread-renderer", {"class": "style-scope ytd-item-section-renderer"}, limit = 5)
+        timer += 1
+        if timer == 3:
+            print('--',getTheVideoTitle(soup), '-- takes to much time to load the comments, maybe there is no comments')
+            break
 
+    for comment in commentsList:
+        allTheComments.append(comment.find("yt-formatted-string", {"id": "content-text"}).text)
+
+    return allTheComments
 
 def createADictionnaryWithAllTheInformations(video_id : str)-> dict:
     if tryWithTheComments:
@@ -89,36 +104,53 @@ def createADictionnaryWithAllTheInformations(video_id : str)-> dict:
     }
 
 
-def writeTheOutputFile(dictionary : dict):
-    filename = 'output.json'
+def writeTheOutputFile(dictionary : dict, outputFile : str):
 
-    with open(filename, "r") as file:
+    with open(outputFile, "r") as file:
         lst = json.load(file)
 
-    with open(filename, mode='w') as f:
+    with open(outputFile, mode='w') as f:
         json.dump(lst, f, ensure_ascii=False)
 
-    with open(filename, mode='w') as f:
+    with open(outputFile, mode='w') as f:
         lst.append(dictionary)
         json.dump(lst, f, ensure_ascii=False)
 
 
-def doTheJob(id):
+def doTheJob(id, outputFile):
     dictionary = createADictionnaryWithAllTheInformations(id)
-    writeTheOutputFile(dictionary)
+    writeTheOutputFile(dictionary, outputFile)
 
 
-def getTheInputFile(inputFile: str):
+def getTheInputFile(inputFile: str, outputFile: str):
     toto = open(inputFile, "r")
     data = json.load(toto)
     for i in data['videos_id']:
-        doTheJob(i)
+        doTheJob(i, outputFile)
     toto.close()
 
 
+def readArguments(argv):
+    inputFile = ''
+    outputFile = ''
+    try:
+        options, args = getopt.getopt(argv,"hi:o:",["input=","output="])
+    except getopt.GetoptError:
+        print ('scrapper.py --input <inputfile> --output <outputfile>')
+        sys.exit(2)
+    for opt, arg in options:
+        if opt == '-h':
+            print ('scrapper.py --input <inputfile> --output <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            inputFile = arg
+        elif opt in ("-o", "--output"):
+            outputFile = arg
+    return inputFile, outputFile
+
 def main():
-    getTheInputFile('input.json')
-    print('toto')
+    inputFile, outputFile =  readArguments(sys.argv[1:])
+    getTheInputFile(inputFile, outputFile)
 
 if __name__ == '__main__':
     main()
---
+++ tests/tests_scrapper.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+
+import scrapper
+import requests
+from bs4 import BeautifulSoup
+import re
+import json as json
+
+response=requests.get("https://www.youtube.com/watch?v=fmsoym8I-3o")
+soup = BeautifulSoup(response.text,"html.parser")
+
+#test getRequestedUrl
+def test_getRequestedUrl():
+    assert scrapper.getTheRequestedUrl("fmsoym8I-3o").status_code == 200
+
+#test transformVideoIntoSoup
+def test_transformVideoIntoSoup():
+    assert type(scrapper.transformTheVideoIntoSoup("fmsoym8I-3o")) == BeautifulSoup
+
+#test getVideoTitle
+def test_getVideoTitle():
+    assert scrapper.getTheVideoTitle(soup) == "Pierre Niney : L’interview face cachée par HugoDécrypte"
+
+#the views can increase so the test can fail
+def test_getVideoViews():
+    assert scrapper.getTheVideoViews(soup) >= "738000"
+
+#test getVideoId
+def test_getVideoId():
+    assert scrapper.getTheVideoId(soup) == "fmsoym8I-3o"
+
+#test getVideoDescription
+def test_getVideoDescription():
+    assert isinstance(scrapper.getTheVideoDescription(soup), str)
+
+#test getVideoChannelName
+def test_getVideoChannelName():
+    assert scrapper.getTheVideoChannelName(soup) == "HugoDécrypte"
+
+#test getVideoNumberOfLikes
+def test_getVideoNumberOfLikes():
+    assert scrapper.getTheVideoNumberOfLikes(soup) >= "0"
+
+#test getExternalLinksWithinVideoDescription
+def test_getExternalLinksWithinVideoDescription():
+    assert isinstance(scrapper.getTheExternalLinksWithinVideoDescription(soup), list)
+
+#test getVideoComments
+def test_getVideoComments():
+    assert isinstance(scrapper.getTheVideoComments("fmsoym8I-3o"), list)
+
+#test createADictionnaryWithAllTheInformations
+def test_createADictionnaryWithAllTheInformations():
+    assert isinstance(scrapper.createADictionnaryWithAllTheInformations("fmsoym8I-3o"), dict)
+
+#test writeTheOutputFile
+def test_writeTheOutputFile():
+    scrapper.writeTheOutputFile("fmsoym8I-3o", 'output.json')
+    assert open('output.json', 'r')
+
+def test_doTheJob():
+    scrapper.doTheJob("fmsoym8I-3o", 'output.json')
+    assert open('output.json', 'r')
+    assert isinstance(scrapper.createADictionnaryWithAllTheInformations('fmsoym8I-3o'), dict)
+
+#test getTheInputFile
+def test_getTheInputFile():
+    assert isinstance(scrapper.getTheInputFile('input.json'), list)
